25bf1e4e8e7007b8248bc87de4cbe1cf18edf0c1
Muthiira <calebmuchiri48@gmail.com>
2022-09-22 21:44:33 +0300

    done

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,14 @@
 import React from "react";
+import react, { useState } from "react";
+
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setisOn] = useState(false);
+   function handleClick (){
+    setisOn((isOn) => !isOn);
+   }
+
+  return <button onClick={handleClick}>{isOn? "ON" : "OFF"}</button>;
 }
 
 export default Toggle;
